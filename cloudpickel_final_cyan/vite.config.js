6cf17ca174519642581f4aa9c5ea6cc2c2919a14
vittal rao r <vittalvarun96@gmail.com>
2025-11-04 17:49:14 +0530

    Update vite.config.js

--- cloudpickel_final_cyan/vite.config.js
+++ cloudpickel_final_cyan/vite.config.js
@@ -1,3 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins:[react()], server:{port:5173} });
+
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 },
+  base: '/', // 👈 important for correct routing on Vercel
+});
